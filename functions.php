a9c104840c2fc445afb627073daf86ea7e387690
Ejner Galaz <jota.ejner@gmail.com>
2017-02-24 14:53:53 -0300

    Corrección coki_url_link()

--- functions.php
+++ functions.php
@@ -5,16 +5,19 @@
  * Diseño para blogs personales. Simple, moderno, potente y ligero.
  * Licenciado bajo GNU/GPL v3
  * Basado en HTML5 Blank por Todd Motto https://toddmotto.com/
+ *
+ * @package Coki
+ * @version 1.0
+ * @since 1.0.0
  */
 
 /**
- * function coki_version()
- *
  * Devuelve la versión actual del theme
+ *
  * @since 1.0.0
  */
 function coki_version() {
-    $theme = wp_get_theme();
+	$theme = wp_get_theme();
 	return $theme->get( 'Version' );
 }
 
@@ -28,9 +31,8 @@ if ( version_compare( $GLOBALS['wp_version'], '4.7-alpha', '<' ) ) {
 require get_template_directory() . '/inc/chat-format.php';
 
 /**
- * coki_setup()
- *
  * Define parámetros básicos del theme y desactiva funciones del núcleo que no se usarán
+ *
  * @since 1.0.0
  */
 function coki_setup() {
@@ -61,35 +63,32 @@ function coki_setup() {
 add_action( 'after_setup_theme', 'coki_setup' );
 
 /**
- * function coki_enqueue()
+ * Carga estilos del tema
  *
- * Carga estilos del theme
  * @since 1.0.0
  */
 function coki_enqueue() {
-	
 	// Normalize 5.0.0
-    wp_register_style( 'normalize', get_template_directory_uri() . '/css/normalize.min.css', array(), '5.0.0', 'all' );
-    wp_enqueue_style( 'normalize' );
-	
+	wp_register_style( 'normalize', get_template_directory_uri() . '/css/normalize.min.css', array(), '5.0.0', 'all' );
+	wp_enqueue_style( 'normalize' );
+
 	// Coki
-    wp_register_style( 'coki', get_template_directory_uri() . '/style.css', array(), coki_version(), 'all' );
-    wp_enqueue_style( 'coki' );
-	
+	wp_register_style( 'coki', get_template_directory_uri() . '/style.css', array(), coki_version(), 'all' );
+	wp_enqueue_style( 'coki' );
+
 	// Coki Icons
-    wp_register_style('coki-font', get_template_directory_uri() . '/css/fonts.css', array(), coki_version(), 'all' );
-    wp_enqueue_style('coki-font');
-	
+	wp_register_style('coki-font', get_template_directory_uri() . '/css/fonts.css', array(), coki_version(), 'all' );
+	wp_enqueue_style('coki-font');
+
 	// Modernizr 3.3.1
-    wp_register_script( 'modernizr', get_template_directory_uri() . '/js/modernizr.min.js', array(), '3.3.1', 'all' );
-    wp_enqueue_script( 'modernizr' );
+	wp_register_script( 'modernizr', get_template_directory_uri() . '/js/modernizr.min.js', array(), '3.3.1', 'all' );
+	wp_enqueue_script( 'modernizr' );
 }
 add_action('wp_enqueue_scripts', 'coki_enqueue');
 
 /**
- * function coki_menu()
- *
  * Crea menú personalizado
+ *
  * @since 1.0.0
  */
 function coki_menu() {
@@ -116,97 +115,99 @@ function coki_menu() {
 }
 
 /**
- * function coki_register_menu()
- *
  * Registra menú personalizado
+ *
  * @since 1.0.0
  */
 function coki_register_menu() {
-    register_nav_menus(array( // Using array to specify more menus if needed
-        'header-menu' => __('Header Menu', 'html5blank'), // Main Navigation
-    ));
+	register_nav_menus(array( // Using array to specify more menus if needed
+		'header-menu' => __('Header Menu', 'html5blank'), // Main Navigation
+	));
 }
 add_action('init', 'coki_register_menu');
 
 /**
- * function coki_pagination( string $prev, string $next )
- *
  * Paginación en entradas, sin dependencias
+ *
  * @since 1.0.0
+ *
+ * @param string $prev Enlace "Anterior"
+ * @param string $next Enlace "Siguiente"
  */
 function coki_pagination( $prev = '<i class="coki-prev"></i>', $next = '<i class="coki-next"></i>' ) {
-    global $wp_query;
-	
-    $big = 999999999;
-	
-    echo paginate_links( array(
-        'base' => str_replace( $big, '%#%', get_pagenum_link( $big ) ),
-        'format' => '?paged=%#%',
-        'current' => max( 1, get_query_var( 'paged' ) ),
-        'total' => $wp_query->max_num_pages,
+	global $wp_query;
+	$big = 999999999;
+
+	echo paginate_links( array(
+		'base' => str_replace( $big, '%#%', get_pagenum_link( $big ) ),
+		'format' => '?paged=%#%',
+		'current' => max( 1, get_query_var( 'paged' ) ),
+		'total' => $wp_query->max_num_pages,
 		'prev_text' => $prev,
 		'next_text' => $next
-    ));
+	));
 }
 add_action( 'init', 'coki_pagination' );
 
 /**
- * function coki_slug_body( string $classes )
- *
  * Añade slug como clase en body (creado por Starkers WordPress Theme)
+ *
  * @since 1.0.0
+ *
+ * @param string $classes Ingresa las clases a filtrar
  */
 function coki_slug_body( $classes ) {
-    global $post;
-	
-    if ( is_home() ) {
-        $key = array_search( 'blog', $classes );
+	global $post;
 
-        if ( $key > -1 ) {
-            unset( $classes[$key] );
-        }
-		
-    } elseif ( is_page() ) {
-        $classes[] = sanitize_html_class( $post->post_name );
-		
-    } elseif ( is_singular() ) {
-        $classes[] = sanitize_html_class( $post->post_name );
-    }
-	
-    return $classes;
+	if ( is_home() ) {
+		$key = array_search( 'blog', $classes );
+
+		if ( $key > -1 ) {
+			unset( $classes[$key] );
+		}
+
+	} elseif ( is_page() ) {
+		$classes[] = sanitize_html_class( $post->post_name );
+
+	} elseif ( is_singular() ) {
+		$classes[] = sanitize_html_class( $post->post_name );
+	}
+
+	return $classes;
 }
 add_filter( 'body_class', 'coki_slug_body' );
 
 /**
- * function coki_icon( string $icon, string $color )
- *
  * Muestra icono según tipo de formato de entrada
+ *
  * @since 1.0.0
+ *
+ * @param string $icon Icono a mostrar
+ * @param string $color Color de fondo a mostrar
  */
 function coki_icon( $icon = '', $color = '' ) {
-	
 	$format = array( 'aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat' );
-	
+
 	if( $icon !== '' ) {
 		$format = $icon;
 	}
-	
+
 	elseif( is_sticky() ) {
 		$format = 'sticky';
 	}
-	
+
 	elseif( has_post_format( $format ) ) {
 		$format = get_post_format();
 	}
-	
+
 	else {
 		$format = 'standard';
 	}
 
-    if( $color !== '' ) {
-        $color = ' style="background-color: ' . $color . '!important"';
-    }
-	
+	if( $color !== '' ) {
+		$color = ' style="background-color: ' . $color . '!important"';
+	}
+
 	echo '<i class="type coki-' . $format . '"' . $color . '></i>';
 }
 
@@ -234,22 +235,33 @@ function coki_time_published() {
 	$time = human_time_diff( get_the_time( 'U' ), current_time( 'timestamp' ) );
 	$time_difference = current_time( 'timestamp' ) - get_the_time( 'U' );
 	$tag = 'class="date" datatime="' . get_the_time( 'Y-m-d\TH:i:s' ) . '"';
-	
+
+	// Si la diferencia es menor a 86400 segundos (24 horas)
 	if ( $time_difference < 86400 ) {
 		echo sprintf( __( '<time %1s>Hace %2$s atrás</time>', 'coki' ), $tag, $time );
+	
+	// Si es mayor a 86400 segundos
 	} else {
 		echo sprintf( __( '<time %1$s>%2$s</time>', 'coki' ), $tag, get_the_date() );
 	}
 }
 
 /**
- * Devuelve primera URL de un post
+ * Devuelve primera URL de un Format Post Link
  *
  * @since 1.0.0
  */
 function coki_url_link() {
 	if ( has_post_format( 'link' ) ) {
-		preg_match( '<a[^>]*href="([^"]*)"[^>]*>', get_the_content(), $matches );
-		return esc_url_raw( $matches[1] );
+		$content = get_the_content();
+
+		// Verifica si existe una etiqueta <a> y extrae el enlace
+		if ( get_url_in_content( $content ) ) {
+			echo get_url_in_content( $content );
+
+		// Si no existe, verifica que exista un enlace en el contenido
+		} elseif ( preg_match( '/(http|https)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/', $content, $matches ) ) {
+			echo esc_url( $matches[0] );
+		}		
 	}
 }
