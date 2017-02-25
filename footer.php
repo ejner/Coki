e28287bdb5a2ee73115d95d17cc531b638d2f54c
Ejner Galaz <jota.ejner@gmail.com>
2017-02-25 01:20:06 -0300

    Ya es un tema diferente

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 
 				<!-- .copyright -->
 				<p class="copyright">
-					<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Licencia Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br /> Potenciado con <a href="//wordpress.org" title="WordPress">WordPress</a>, diseño basado en <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
+					<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Licencia Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br /> Potenciado con <a href="//wordpress.org" title="WordPress">WordPress</a> y <a href="//github.com/ejner/Coki" title="Coki Theme">Coki</a>.
 				</p>
 				<!-- /.copyright -->
 
