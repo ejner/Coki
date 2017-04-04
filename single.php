83a2f88f37f3f83f9a6fd5028b0c2036afc7e27f
Ejner Alfredo Galaz <Ejner Alfredo Galaz>
2017-04-04 01:35:20 -0300

    Ajustes

--- single.php
+++ single.php
@@ -16,7 +16,7 @@ get_header(); ?>
 
 		<?php
 		if ( have_posts() ) : while ( have_posts() ) : the_post();
-			
+
 			get_template_part( 'loop' ); // Carga plantilla del loop.
 			comments_template(); // Carga comentarios.
 
